e8bc9fba071c1354c169e52b7059e5d289bfcbd4
fizk <einar@99designs.com>
2015-08-07 18:34:17 +1000

    Making server file cleaner

--- src/server/server.js
+++ src/server/server.js
@@ -4,19 +4,13 @@ import Router from "react-router";
 import Swig from "swig";
 const app = express();
 
-// This is where all the magic happens!
 app.engine('html', Swig.renderFile);
 
 app.set('view engine', 'html');
 app.set('views', __dirname + '/../../views');
-
-// Swig will cache templates for you, but you can disable
-// that and use Express's caching instead, if you like:
 app.set('view cache', false);
-// To disable Swig's cache, do the following:
 Swig.setDefaults({ cache: false });
 // NOTE: You should always cache templates in a production environment.
-// Don't leave both of these to `false` in production!
 
 import routes from "../shared/routes";
 
@@ -28,8 +22,9 @@ app.get('/*', function (req, res) {
 });
 
 var server = app.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log(
+        'Server listening at http://%s:%s',
+        server.address().address,
+        server.address().port
+    );
 });
